14967d21a8baf780172b5b4edd0fd868fa924ac9
Niels Voss <code@nielsvoss.com>
2026-03-22 00:59:43 -0700

    Use an API that more closely matches Mat_

--- Ainfinity/AdditiveCompletion.lean
+++ Ainfinity/AdditiveCompletion.lean
@@ -2,17 +2,44 @@ import Mathlib
 
 open CategoryTheory
 
+-- TODO: Consider renaming to something like `ComputableMat_` or `CMat_`
+
 structure AdditiveCompletion (C : Type*) where
   ofList ::
     toList : List C
 
-abbrev AdditiveCompletion.length {C : Type*} (X : AdditiveCompletion C) : ℕ :=
-  X.toList.length
-
 variable {R : Type*} [CommRing R]
   {C : Type*} [Category C] [Preadditive C] [Linear R C]
+  (M N : AdditiveCompletion C)
+
+/-- Mirrors the API of `CategoryTheory.Mat_.ι` -/
+def AdditiveCompletion.ι : Type := Fin M.toList.length
+
+/- Auxillary definitions that are used to avoid defeq abuse. -/
+def AdditiveCompletion.ι.toFin (i : M.ι) : Fin M.toList.length := i
+def AdditiveCompletion.ι.ofFin (i : Fin M.toList.length) : M.ι := i
+
+instance AdditiveCompletion.fintype : Fintype M.ι := inferInstanceAs (Fintype (Fin M.toList.length))
+instance : DecidableEq M.ι := inferInstanceAs (DecidableEq (Fin M.toList.length))
+
+/-- Mirrors the API of `CategoryTheory.Mat_.X` -/
+def AdditiveCompletion.X : M.ι → C := fun i ↦ M.toList[i.toFin]
+
+/-- Mirrors the API of `CategoryTheory.Mat_.Hom` -/
+def AdditiveCompletion.Hom : Type* := DMatrix M.ι N.ι fun i j => M.X i ⟶ N.X j
+
+/-- Mirrors the API of `CategoryTheory.Mat_.id` -/
+def AdditiveCompletion.id : Hom M M :=
+  fun i j => if h : i = j then eqToHom (congr_arg M.X h) else 0
+
+/-- Mirrors the API of `CategoryTheory.Mat_.comp` -/
+def AdditiveCompletion.comp {M N K : AdditiveCompletion C} (f : Hom M N) (g : Hom N K) : Hom M K :=
+  fun i k => ∑ j : N.ι, f i j ≫ g j k
 
 instance : Category (AdditiveCompletion C) where
-  Hom X Y := DMatrix (Fin X.length) (Fin Y.length) (fun i j ↦ X.toList[i] ⟶ Y.toList[j])
-  id X i j := if h : i = j then eqToHom (congrArg _ h) else 0
-  comp {X Y Z} f g := g * f
+  Hom := AdditiveCompletion.Hom
+  id := AdditiveCompletion.id
+  comp := AdditiveCompletion.comp
+  id_comp := sorry
+  comp_id := sorry
+  assoc := sorry
