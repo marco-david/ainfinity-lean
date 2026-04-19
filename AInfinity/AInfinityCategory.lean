6b64f18cfda8a98b505115ce6738165a57fa96b7
Justin Mu <justinmu2007@gmail.com>
2026-04-18 17:38:53 -0700

    precodex

--- AInfinity/AInfinityCategory.lean
+++ AInfinity/AInfinityCategory.lean
@@ -19,12 +19,12 @@ class AInfinityCategoryData
     (R : Type u) [CommRing R] (Obj : Type w)
     extends RLinearGQuiver (β := β) R Obj where
   m :
-    {n : ℕ+} →
+    {n : ℕ} → [NeZero n] →
     (obj : Fin ((n : ℕ) + 1) → Obj) →
     (deg : Fin (n : ℕ) → β) →
     MultilinearMap R
-      (fun i : Fin (n : ℕ) => composableHomType toRLinearGQuiver.Hom obj deg i)
-      (operationTargetType toRLinearGQuiver.Hom obj deg)
+      (fun i : Fin (n : ℕ) => composableHomType (GHom β R) obj deg i)
+      (operationTargetType (GHom β R) obj deg)
 
 namespace AInfinityCategoryData
 
@@ -34,11 +34,11 @@ variable (Obj : Type w)
 /-- The full Stasheff sum in arity n, with Koszul signs. -/
 def stasheffSum
     (X : AInfinityCategoryData β R Obj)
-    {n : ℕ+}
-    (obj : Fin ((n : ℕ) + 1) → Obj)
-    (deg : Fin (n : ℕ) → β)
-    (x : ∀ i : Fin (n : ℕ), composableHomType X.Hom obj deg i) :
-    X.Hom (obj 0) (obj (Fin.last (n : ℕ))) (stasheffTargetDeg deg) :=
+    {n : ℕ}
+    (obj : Fin (n + 1) → Obj)
+    (deg : Fin n → β)
+    (x : ∀ i : Fin n, composableHomType X.Hom obj deg i) :
+    X.Hom (obj 0) (obj (Fin.last n)) (stasheffTargetDeg deg) :=
   indexedStasheffSum X.Hom X.m obj deg x
 
 /-- The Stasheff identities as a property of the raw A∞ category data. -/
