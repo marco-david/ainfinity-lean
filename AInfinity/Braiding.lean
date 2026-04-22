d8868bbe9a2119c975fe51fc6de9a3678a842dbd
Hydroxyi <jasondong1@berkeley.edu>
2026-04-22 16:43:20 -0700

    braiding last changes-changed notes

--- AInfinity/Braiding.lean
+++ AInfinity/Braiding.lean
@@ -15,7 +15,7 @@ structure BraidingFunctorData (R : Type u) [CommRing R] [CharP R 2] [DecidableEq
   gen₀ : KLRWCategory n R → CochainComplex (CMat_ (KLRWCategory n R)) ℤ
   gen₁ : {A B : KLRWCategory n R} → (A ⟶ B) → (gen₀ A ⟶ gen₀ B)
   gen₂ : {A B C : KLRWCategory n R} → (A ⟶ B) → (B ⟶ C) → ∀ (i : ℤ), (gen₀ A).X i → (gen₀ B).X (i - 1)
-  -- NOTE! Replaced this with a degree -1 graded map, instead of another chain map
+  -- invoke shift functor from boundedcochaincomplexes
 
   --SFₙ axioms. Note:
   --KLRW is a preadditive category, so its Hom-space is
