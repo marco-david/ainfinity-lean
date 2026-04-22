88b4a4f6887a092344d66ba9a9a1bc1a6f66ae1b
Hydroxyi <jasondong1@berkeley.edu>
2026-04-22 15:27:34 -0700

    leaned SF axioms

--- AInfinity/Braiding.lean
+++ AInfinity/Braiding.lean
@@ -14,8 +14,26 @@ variable {R : Type u} [CommRing R] [CharP R 2] [DecidableEq R] {n : ℕ}
 structure BraidingFunctorData (R : Type u) [CommRing R] [CharP R 2] [DecidableEq R] (n : ℕ) where
   gen₀ : KLRWCategory n R → CochainComplex (CMat_ (KLRWCategory n R)) ℤ
   gen₁ : {A B : KLRWCategory n R} → (A ⟶ B) → (gen₀ A ⟶ gen₀ B)
-  gen₂ : {A B C : KLRWCategory n R} → (A ⟶ B) → (B ⟶ C) → (gen₀ A ⟶ gen₀ C)
+  gen₂ : {A B C : KLRWCategory n R} → (A ⟶ B) → (B ⟶ C) → ∀ (i : ℤ), (gen₀ A).X i → (gen₀ B).X (i - 1)
+  -- Replaced this with a degree -1 graded map, instead of another chain map
 
+  --SFₙ axioms. Note:
+  --KLRW is a preadditive category, so its Hom-space is
+  --all degree 0. Then only μ₂ is nonzero (it is composition).
+  --Tw(Add(KLRW)) is a dg-category, so it has μ₁ = d, μ₂ = composition,
+  --and no higher terms. 
+  SF₂ : ∀ {A B C : KLRWCategory n R} (f : A ⟶ B) (g : B ⟶ C) (i : ℤ),
+    (gen₁ (f ≫ g)).f i + (gen₁ f ≫ gen₁ g).f i
+      = gen₂ f g i ≫ (gen₀ C).d (i - 1) i
+        + (gen₀ A).d i (i + 1) ≫ gen₂ f g (i + 1)
+  SF₃ : ∀ {A B C D : KLRWCategory n R}
+    (f : A ⟶ B) (g : B ⟶ C) (h : C ⟶ D) (i : ℤ),
+    gen₂ (f ≫ g) h i + gen₂ f (g ≫ h) i
+      = (gen₁ f).f i ≫ gen₂ g h i
+        + gen₂ f g i ≫ (gen₁ h).f (i - 1)
+  SF₄ : ∀ {A B C D E : KLRWCategory n R}
+    (f : A ⟶ B) (g : B ⟶ C) (h : C ⟶ D) (k : D ⟶ E) (i : ℤ),
+    gen₂ f g i ≫ gen₂ h k (i - 1) = 0
 namespace BraidingFunctorData
 
 variable (β : BraidingFunctorData R n)
