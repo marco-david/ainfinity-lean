d8eaafb4759d38e25b6f97ca44fae477c0512c40
Niels Voss <code@nielsvoss.com>
2026-04-26 02:32:22 -0700

    Start working on constructors for binary biproduct data

--- AInfinity/ComputableCategories.lean
+++ AInfinity/ComputableCategories.lean
@@ -6,6 +6,30 @@ public import Mathlib
 
 open CategoryTheory Limits
 
+abbrev BinaryBiproductData.mkOfMaps {C : Type*} [Category C] [HasZeroMorphisms C] (P Q : C)
+    (pt : C) (fst : pt ⟶ P) (snd : pt ⟶ Q) (inl : P ⟶ pt) (inr : Q ⟶ pt)
+    (pair : ∀ X, (X ⟶ P) → (X ⟶ Q) → (X ⟶ pt)) (copair : ∀ X, (P ⟶ X) → (Q ⟶ X) → (pt ⟶ X))
+    (inl_fst : inl ≫ fst = 𝟙 P := by aesop) (inl_snd : inl ≫ snd = 0 := by aesop)
+    (inr_fst : inr ≫ fst = 0 := by aesop) (inr_snd : inr ≫ snd = 𝟙 Q := by aesop)
+    (pair_fst : ∀ X (f : X ⟶ P) (g : X ⟶ Q), pair X f g ≫ fst = f := by aesop)
+    (pair_snd : ∀ X (f : X ⟶ P) (g : X ⟶ Q), pair X f g ≫ snd = g := by aesop)
+    (inl_copair : ∀ X (f : P ⟶ X) (g : Q ⟶ X), inl ≫ copair X f g = f := by aesop)
+    (inr_copair : ∀ X (f : P ⟶ X) (g : Q ⟶ X), inr ≫ copair X f g = g := by aesop)
+    : BinaryBiproductData P Q where
+  bicone := ⟨pt, fst, snd, inl, inr, inl_fst, inl_snd, inr_fst, inr_snd⟩
+  isBilimit := {
+    isLimit := {
+      lift s := sorry
+      fac s j := sorry
+      uniq s m := sorry
+    }
+    isColimit := {
+      desc s := sorry
+      fac s j := sorry
+      uniq s m := sorry
+    }
+  }
+
 class ComputableBinaryBiproduct (C : Type*) [Category C] [HasZeroMorphisms C] where
   computableBinaryBiproductData (P Q : C) : BinaryBiproductData P Q
 
@@ -37,3 +61,5 @@ abbrev cbiprod.fst {X Y : C} : X ⊞ᶜ Y ⟶ X := (computableBinaryBiproductDat
 abbrev cbiprod.snd {X Y : C} : X ⊞ᶜ Y ⟶ Y := (computableBinaryBiproductData X Y).bicone.snd
 abbrev cbiprod.inl {X Y : C} : X ⟶ X ⊞ᶜ Y := (computableBinaryBiproductData X Y).bicone.inl
 abbrev cbiprod.inr {X Y : C} : Y ⟶ X ⊞ᶜ Y := (computableBinaryBiproductData X Y).bicone.inr
+
+noncomputable def cbiprod_iso_biprod : cbiprod X Y ≅ biprod X Y := sorry
