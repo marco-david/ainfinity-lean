0ca75b0679679eea4a8b0f19e8f660ef997911f9
Niels Voss <code@nielsvoss.com>
2026-04-29 02:56:26 -0700

    Clean up AInfinity.lean file using lake exe mk_all
    This should hopefully make GitHub actions happy

--- AInfinity.lean
+++ AInfinity.lean
@@ -1,8 +1,7 @@
 import AInfinity.AInfinityAlgebra
-import AInfinity.AdditiveCompletion
-import AInfinity.BoundedCochainComplex
 import AInfinity.AInfinityCategory
 import AInfinity.AdditiveCompletion
+import AInfinity.BoundedCochainComplex
 import AInfinity.Grading
 import AInfinity.KLRW
 import AInfinity.Stasheff
