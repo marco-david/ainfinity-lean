28cfaebe86cb9c2794a47f36cb362f2391775b3a
Niels Voss <code@nielsvoss.com>
2026-04-29 02:46:15 -0700

    Resolve the merge conflict properly
    I forgot to add the file in the last commit

--- AInfinity.lean
+++ AInfinity.lean
@@ -1,11 +1,8 @@
 import AInfinity.AInfinityAlgebra
-<<<<<<< HEAD
 import AInfinity.AdditiveCompletion
 import AInfinity.BoundedCochainComplex
-=======
 import AInfinity.AInfinityCategory
 import AInfinity.AdditiveCompletion
->>>>>>> origin/master
 import AInfinity.Grading
 import AInfinity.KLRW
 import AInfinity.Stasheff
